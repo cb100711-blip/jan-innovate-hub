86f24aa01295fd469ba206258141aa01de22dff6
cb100711-blip <cb100711@gmail.com>
2026-09-15 01:21:46 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const multer = require("multer");
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 // ======================================
 // MIDDLEWARE
